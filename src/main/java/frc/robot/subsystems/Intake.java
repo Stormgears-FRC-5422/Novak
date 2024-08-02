11adf7a6525cb9364d3fb373c0ea6e5eec102795
Allison <allison.t.luo@gmail.com>
2024-08-01 20:42:09 -0400

    Testing encoder

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -4,9 +4,8 @@
 
 package frc.robot.subsystems;
 
-import com.revrobotics.CANSparkLowLevel;
+import com.revrobotics.*;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import com.revrobotics.CANSparkMax;
 import frc.robot.Constants;
 
 public class Intake extends SubsystemBase {
@@ -17,6 +16,8 @@ public class Intake extends SubsystemBase {
   }
 
   private final CANSparkMax intakeMotor;
+  private final CANSparkMax test;
+  private final RelativeEncoder m_alternateEncoder;
   private double intakeMotorSpeed;
   private IntakeState intakeState;
 
@@ -24,6 +25,8 @@ public class Intake extends SubsystemBase {
   public Intake() {
     // used fake id, change to real motor id later
     intakeMotor = new CANSparkMax(Constants.Intake.intakeID, CANSparkLowLevel.MotorType.kBrushless);
+    test = new CANSparkMax(9, CANSparkLowLevel.MotorType.kBrushless);
+    m_alternateEncoder = test.getAlternateEncoder(SparkMaxAlternateEncoder.Type.kQuadrature, 8192);
     intakeMotor.setInverted(true);
     setIntakeState(IntakeState.OFF);
   }
@@ -36,6 +39,9 @@ public class Intake extends SubsystemBase {
   public void periodic() {
     // This method will be called once per scheduler run
     intakeMotor.set(intakeMotorSpeed);
+    System.out.println("alr encoder position: " + m_alternateEncoder.getPosition());
+    System.out.println("alr encoder velocity: " + m_alternateEncoder.getVelocity());
+
 
   }
 
