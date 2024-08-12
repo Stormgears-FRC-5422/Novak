3cf5fd36e582b310fa4367a970bee749f1d9efae
mpillai2 <meher.pillai@gmail.com>
2024-08-11 20:59:28 -0400

    Gate code

--- src/main/java/frc/robot/commands/ShootCommand.java
+++ src/main/java/frc/robot/commands/ShootCommand.java
@@ -1,9 +1,12 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.subsystems.Shooter;
 import frc.robot.subsystems.Storage;
 import org.littletonrobotics.junction.AutoLogOutput;
+import frc.robot.subsystems.Storage;
+
 
 public class ShootCommand extends Command {
     @AutoLogOutput(key = "shooter_speed")
@@ -21,13 +24,24 @@ public class ShootCommand extends Command {
     
     public void initialize() {
         count = 0;
+//        storage.setRelayState(Storage.RelayState.CLOSE);
+
         shooter.setShooterState(Shooter.ShooterState.FORWARD);
         storage.setStorageState(Storage.StorageState.FORWARD);
+
+        new WaitCommand(1);
+
+        storage.setRelayState(Storage.RelayState.OPEN);
+
+        new WaitCommand(2);
+
+        storage.setRelayState(Storage.RelayState.CLOSE);
+
     }
 
     @Override
     public boolean isFinished() {
-        return count >= 100;
+        return count >= 150;
     }
 
     @Override
--- src/main/java/frc/robot/subsystems/Storage.java
+++ src/main/java/frc/robot/subsystems/Storage.java
@@ -2,8 +2,13 @@ package frc.robot.subsystems;
 
 import com.revrobotics.CANSparkLowLevel;
 import com.revrobotics.CANSparkMax;
+import edu.wpi.first.units.Voltage;
+import edu.wpi.first.wpilibj.Relay;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.Constants;
+import frc.utils.motorcontrol.StormSpark;
+import frc.utils.motorcontrol.StormSpark;
 
 
 
@@ -14,23 +19,55 @@ public class Storage extends SubsystemBase {
         REVERSE,
     }
 
-    final CANSparkMax storageMotor;
-    double storageMotorSpeed;
-    StorageState storageState;
+    public enum RelayState {
+        IDLE,
+        CLOSE,
+        OPEN
 
-    public Storage() {
-        storageMotor = new CANSparkMax(Constants.Storage.storageID, CANSparkLowLevel.MotorType.kBrushless);
-        storageMotor.setInverted(false);
-        setStorageState(StorageState.OFF);
     }
 
-    void setSpeed(double speed) {
+
+
+    private final CANSparkMax storageMotor;
+    private double storageMotorSpeed;
+
+    private double voltage;
+    private StorageState storageState;
+
+    private RelayState relaystate;
+
+    private final CANSparkMax relayspark;
+
+    private int m_currentLimit;
+
+    public Storage() {
+        storageMotor = new StormSpark(Constants.Storage.storageID, StormSpark.MotorType.kBrushed, StormSpark.MotorKind.kNeo);
+        storageMotor.setInverted(true);
+        setStorageState(StorageState.OFF);
+        relayspark = new CANSparkMax(Constants.RelayID, CANSparkLowLevel.MotorType.kBrushed);
+        m_currentLimit = Constants.CurrentLimitAmps;
+        relayspark.setSmartCurrentLimit(m_currentLimit);
+
+        voltage = 0.0;
+
+    }
+
+    public void setVoltage(double outputVolts) {
+        this.voltage = outputVolts;
+    }
+
+
+    public void setSpeed(double speed) {
         storageMotorSpeed = speed;
     }
 
+
     public void periodic() {
         storageMotor.set(storageMotorSpeed);
+        relayspark.setVoltage(voltage);
     }
+
+
 
     public void setStorageState(StorageState state) {
         this.storageState = state;
@@ -43,6 +80,33 @@ public class Storage extends SubsystemBase {
             }
             case REVERSE -> {
                 setSpeed(-Constants.Storage.storageSpeed);
+            }
+        }
+    }
+
+    public void setRelayState(RelayState state) {
+        this.relaystate = state;
+        switch (state) {
+            case IDLE -> {
+                System.out.println("gate neutralized");
+
+                setVoltage(0.0);
+            }
+            case CLOSE -> {
+                System.out.println("closed gate");
+
+                setVoltage(Constants.Storage.Close);
+
+            }
+            case OPEN -> {
+                System.out.println("gate opened");
+
+                setVoltage(Constants.Storage.Open);
+
+//                new WaitCommand(3.0);
+
+
+
             }
         }
     }
